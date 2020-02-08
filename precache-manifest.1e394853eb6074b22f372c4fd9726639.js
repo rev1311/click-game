39df6aa799a15eb3ea2c5182a04b3c0dd46d0af3
Michael Partin <unmykx@gmail.com>
2020-02-07 21:45:10 -0800

    Updates

---
+++ precache-manifest.1e394853eb6074b22f372c4fd9726639.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30aec57548a9708cf253b28db383d0b0",
+    "url": "/click-game/index.html"
+  },
+  {
+    "revision": "2354baad28be3173151f",
+    "url": "/click-game/static/css/main.367868ab.chunk.css"
+  },
+  {
+    "revision": "c80236b9d62bccc5f7db",
+    "url": "/click-game/static/js/2.1e0f97dc.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/click-game/static/js/2.1e0f97dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2354baad28be3173151f",
+    "url": "/click-game/static/js/main.4a6f2a34.chunk.js"
+  },
+  {
+    "revision": "bc6e8de25b8fc21c18e3",
+    "url": "/click-game/static/js/runtime-main.2bdea4f2.js"
+  }
+]);
