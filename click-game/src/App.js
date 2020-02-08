04d030af4246b5d801cd8d5d21d4843a75bf5acf
Michael Partin <unmykx@gmail.com>
2020-02-07 19:19:26 -0800

    added functionality to app.js

--- click-game/src/App.js
+++ click-game/src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+import Nav from './containers/Navbar';
+import Instructions from './components/Instructions';
+import Card from './components/Card';
+
+import cards from './cards.json';
+
+class App extends Component {
+  state = {
+    score: 0,
+    highScore: 0,
+    picks: [],
+    feedback: 'Click An Image To Play',
+    cards
+  }
+  
+  pointCheck = id => {
+    
+    if (this.state.picks.includes(id)) {
+      if (this.state.highScore < this.state.score) {
+        this.setState({ 
+          highScore: this.state.score
+        })
+      }
+      this.setState({
+        score: 0,
+        feedback: 'Hmm... No.',
+        picks: []
+      })
+    } else {
+      this.setState({
+        score: this.state.score + 1,
+        feedback: 'Toss a coin to your Witcher...',
+        picks: [...this.state.picks, id]
+      })
+    }
+  }
+
+  renderBoard() {
+    const cardDeck = [...this.state.cards];
+
+    cardDeck.sort(() => Math.random() - 0.5);
+
+    return (
+        cardDeck.map(card => {
+          return (
+            <Col xs={3} key={card.id}>
+              <Card
+                id={card.id}
+                name={card.name}
+                image={card.image}
+                handleClick={this.pointCheck}
+              />
+            </Col>
+          )
+        })
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav
+          score={this.state.score}
+          highScore={this.state.highScore}
+          feedback={this.state.feedback} />
+        <Instructions />
+        <Container>
+          <Row className="justify-content-sm-center">
+            <Col xs={10}>
+              <Row noGutters={true}>
+                {this.renderBoard()}
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
